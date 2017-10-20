51a52501d408ed14975d1354224e50ff2099f9df
Paul Ohno <pohno@users.noreply.github.com>
2017-10-20 17:33:50 -0500

    added usb driver control of flip mirror

---
+++ flipmirror.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import ftd2xx
+import ftd2xx.defines as constants
+import time
+
+class FlipMirror:
+
+    # Raw byte commands for "MGMSG_MOT_MOVE_JOG".
+    up_position = b"\x6A\x04\x00\x01\x21\x01"
+    down_position = b"\x6A\x04\x00\x02\x21\x01"
+    
+    #motor object
+    motor = []
+
+    def __init__(self):
+        #serial number for this motor flipper
+        serial = b"37874729"
+        
+  
+        # Recommended d2xx setup instructions from Thorlabs.
+        motor = ftd2xx.openEx(serial)
+        #print(motor.getDeviceInfo())
+        motor.setBaudRate(115200)
+        motor.setDataCharacteristics(constants.BITS_8, constants.STOP_BITS_1, constants.PARITY_NONE)
+        time.sleep(.05)
+        motor.purge()
+        time.sleep(.05)
+        motor.resetDevice()
+        motor.setFlowControl(constants.FLOW_RTS_CTS, 0, 0)
+        motor.setRts()
+        
+        self.motor = motor
+
+    def setPosUp(self):
+        # Send raw bytes to USB driver.
+        self.motor.write(self.down_position)
+    
+    def setPosDown(self):
+        self.motor.write(self.up_position)
+        
+    def close(self):
+        self.motor.close()
