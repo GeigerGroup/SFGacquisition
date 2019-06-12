9a07c76ec8b37cb6ac317b4f4ce657c8458b3772
Paul Ohno <pohno@users.noreply.github.com>
2019-06-12 17:05:48 -0500

    updated for wintopas4
    client server interactions

--- topas.py
+++ topas.py
@@ -1,75 +1,57 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct 21 16:01:41 2017
+Created on Wed Jun 12 15:31:21 2019
 
-Creates object to interact with WinTOPAS software through COM port.
-
-@author: pohno
+@author: Paul Ohno and Yangdongling Liu
 """
-import serial
+
+#add .NET library
+import clr
+clr.AddReference(r"C:\Users\Spectra Physics\Documents\Topas4PublicAPI-master\NET_SDK\Topas4Lib")
+
 import time
+import sys
+import Mint
+
+from Topas4Lib import TopasDevice
 
 class TOPAS():
-
-    def __init__(self):
-        
-        #initialize serial port
-        ser = serial.Serial()
-        
-        #port on computer
-        ser.port = 'COM5'
-        
-        #serial settings
-        ser.bytesize = serial.EIGHTBITS
-        ser.stopbits = serial.STOPBITS_ONE
-        ser.parity = serial.PARITY_NONE
-        ser.xonxoff = False
-        ser.baudrate = 9600
-        
-        #timeout after 1 second so don't wait forever
-        ser.timeout = 1
-        
-        #open serial port
-        ser.open()
-        
-        #set serial port property
-        self.ser = ser
     
-    
-    #verifies that topas is there    
-    def getStatus(self):
-        self.ser.write(b'GetStatus\r')
-        return self.ser.readline()
+    def __init__(self):       
+        serialNumber = "10777" #enter serial number or your own device here
+        self.topas = TopasDevice.FindTopasDevice(serialNumber)
         
-    
-    #gets the current wavelength setting    
-    def getWavelength(self):
-        self.ser.write(b'GetWavelength\r')
-        return self.ser.readline()
- 
-    
-    #sets wavelength with DFG1, diff TOPAS config may need diff interaction code
-    def setWavelength(self,wl):
-        self.ser.write(b'SetWavelengthEx ' + str(wl).encode() + b', 1,0,0,6\r' )
-        
-        #wait for 3 seconds to give it time to move
-        time.sleep(3)
-        return self.ser.readline()
-    
-    
-    #close TOPAS shutter
+        if self.topas is None:
+            print ('Device with serial number %s not found' % serialNumber)
+            
     def closeShutter(self):
-        self.ser.write(b'CloseShutter\r')
+        self.topas.ShutterService.SetOpenCloseShutter(False) 
+        time.sleep(0.5)
         
-        #wait for 1 second to give it time to close
-        time.sleep(1)
-        return self.ser.readline()
-    
-    
-    #open TOPAS shutter
     def openShutter(self):
-        self.ser.write(b'OpenShutter\r')
-        
-        #wait for 1 second to give it time to open
-        time.sleep(1)
-        return self.ser.readline()
+        self.topas.ShutterService.SetOpenCloseShutter(True) 
+        time.sleep(0.5)
+
+    def setWavelength(self,wavelength):
+       #get all interactions; DFG1 = interactions[8], DFG2 = interactions[9]
+       interactions = self.topas.WavelengthService.GetExpandedInteractions()
+       
+       #select DFG1
+       interaction = interactions[8]
+       
+       #set it
+       print("setting wavelength %.4f nm using interaction %s" % (wavelength, interaction.Type))
+       self.topas.SetWavelength(wavelength, interaction.Type)
+       
+       #wait until its done
+       self.waitTillSet()
+
+    def waitTillSet(self):
+        #loop to check if wavelength has been set
+        while(True):
+            #get status
+            s = self.topas.WavelengthService.GetOutput()
+            if s.IsWavelengthSettingInProgress == False:
+                break
+            time.sleep(0.05)
+        print("Done setting wavelength")
